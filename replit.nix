0afacd82fb0754464bad7290cbcaf5c16f9e20a0
fmitaacms <37267323-fmitaacms@users.noreply.replit.com>
2024-11-01 15:30:43 +0000

    Restored to 'da2f4e615297bca6bcda959b53c543dddfe63dab'
    Replit-Restored-To:da2f4e615297bca6bcda959b53c543dddfe63dab

--- replit.nix
+++ replit.nix
@@ -1,11 +1,5 @@
 {pkgs}: {
   deps = [
-    pkgs.docker_26
-    pkgs.findutils
-    pkgs.coreutils
-    pkgs.curlWithGnuTls
-    pkgs.bashInteractive
-    pkgs.datadog-agent
     pkgs.imagemagickBig
     pkgs.ffmpeg-full
   ];
