4e5343032023570635a40d9d2d28c6d2ee2eec17
fmitaacms <37267323-fmitaacms@users.noreply.replit.com>
2024-11-01 15:30:42 +0000

    Checkpoint before revert - No step

--- replit.nix
+++ replit.nix
@@ -1,5 +1,11 @@
 {pkgs}: {
   deps = [
+    pkgs.docker_26
+    pkgs.findutils
+    pkgs.coreutils
+    pkgs.curlWithGnuTls
+    pkgs.bashInteractive
+    pkgs.datadog-agent
     pkgs.imagemagickBig
     pkgs.ffmpeg-full
   ];
